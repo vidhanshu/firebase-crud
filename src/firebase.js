e7e053d156daea023441420e652dc0a2a3b83488
vidhanshu borade <vidhanshu7218555039@gmail.com>
2022-05-28 13:47:18 +0530

    env not working

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // import { getDatabase } from 'firebase/database'
-import { getFirestore } from "firebase/firestore"
+import {getFirestore} from "firebase/firestore"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATA_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyDuNn_3c4aaBXF4O_V3qtaxzDZKEH2OISU",
+  authDomain: "shopping-list-6ca23.firebaseapp.com",
+  databaseURL: "https://shopping-list-6ca23-default-rtdb.firebaseio.com",
+  projectId: "shopping-list-6ca23",
+  storageBucket: "shopping-list-6ca23.appspot.com",
+  messagingSenderId: "535437915945",
+  appId: "1:535437915945:web:719fdbdcc7673db6b2e938"
 };
 
 // Initialize Firebase
